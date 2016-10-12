f586d1fcd2925680a8034f1ae82cb2caec836810
vagrant <abhinandam@gmail.com>
2016-10-12 00:18:43 +0000

    add time to webpage

--- helloflask.py
+++ helloflask.py
@@ -1,9 +1,10 @@
+import time
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "The number of seconds since January 1st, 1970 is " + str(int(time.time()))
 
 if __name__ == "__main__":
     app.run()
